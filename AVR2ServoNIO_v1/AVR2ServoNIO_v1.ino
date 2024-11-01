19132c5276f6801e7d3cfae9e450780cfb432b40
johnmholmes <60571002+johnmholmes@users.noreply.github.com>
2024-11-01 08:38:01 +0000

    Update AVR2ServoNIO_v1.ino

--- AVR2ServoNIO_v1/AVR2ServoNIO_v1.ino
+++ AVR2ServoNIO_v1/AVR2ServoNIO_v1.ino
@@ -41,18 +41,18 @@ the MERG Arduino CAN shield kit 110.
 
 // Allow direct to JMRI via USB, without CAN controller, comment out for CAN
 //    ( Note: disable debugging if this is chosen. )
-//#include "GCSerial.h"
+#include "GCSerial.h"
 
 #include <Wire.h>
 
 // To set a new nodeid edit the next line (0x01 - 0x02 etc JH)
-#define NODE_ADDRESS  5,1,1,1,0x8E,0x01
+#define NODE_ADDRESS  5,1,1,1,0x8E,0x02
 
 /* To Force Reset EEPROM to Factory Defaults set this value t0 1, else 0.
    Need to do this at least once. (On first setup of any module set to 1 upload and 
   then change to 0 JH)
 */
-#define RESET_TO_FACTORY_DEFAULTS 1 // Set to 0 
+#define RESET_TO_FACTORY_DEFAULTS 0 // Set to 0 
 
 // Board definitions
 #define MANU "OpenLCB"      // The manufacturer of node
@@ -213,7 +213,7 @@ uint8_t servopin[NUM_SERVOS] = {A4,A5};
 uint8_t servoActual[NUM_SERVOS] = { 90, 90 };
 uint8_t servoTarget[NUM_SERVOS] = { 90, 90 };
 #ifdef NOCAN
-  uint8_t iopin[NUM_IO] = {13,5,6,9,A0,A1,A2,A3}; // use pin 13 LED for demo purposes
+  uint8_t iopin[NUM_IO] = {4,5,6,9,A0,A1,A2,A3}; // use pin 13 LED for demo purposes
 #else
   uint8_t iopin[NUM_IO] = {4,5,6,7,A0,A1,A2,A3};  // use free pins on MERG CAN board
 #endif
