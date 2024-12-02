eb3b640d1b2107ab67a8b5d6f4c90f08f7d0b9e1
johnmholmes <60571002+johnmholmes@users.noreply.github.com>
2024-12-02 13:23:29 +0000

    Update AVR2Servo8IO_NoCan.ino

--- Examples/No_Can/AVR2Servo8IO_NoCan/AVR2Servo8IO_NoCan.ino
+++ Examples/No_Can/AVR2Servo8IO_NoCan/AVR2Servo8IO_NoCan.ino
@@ -98,7 +98,7 @@ const char configDefInfo[] PROGMEM =
         <group replication=')" N(NUM_POS) R"('>
             <repname>Position</repname>
             <eventid><name>EventID</name></eventid>
-            <int size='2'>
+            <int size='1'>
                 <name>Servo Position in Degrees</name>
                 <min>0</min><max>180</max>
                 <hints><slider tickSpacing='45' immediate='yes'> </slider></hints>
@@ -154,7 +154,7 @@ const char configDefInfo[] PROGMEM =
             char desc[8];        // description of this Servo Turnout Driver
             struct {
               EventID eid;       // consumer eventID
-              uint16_t angle;       // position
+              uint8_t angle;       // position
             } pos[NUM_POS];
           } servos[NUM_SERVOS];
           struct {
@@ -233,7 +233,7 @@ void userInitAll()
     NODECONFIG.put(EEADDR(servos[i].desc), ESTRING(""));
     for(int p=0; p<NUM_POS; p++) {
       //NODECONFIG.put(EEADDR(servos[i].pos[p].angle), (uint8_t)((p*180)/(NUM_POS-1)));
-      NODECONFIG.write16(EEADDR(servos[i].pos[p].angle), 90);
+      NODECONFIG.put(EEADDR(servos[i].pos[p].angle), 90);
     }
   }
   for(uint8_t i = 0; i < NUM_IO; i++) {
