3c83e82794a4f382899617a43ec852e913736254
johnmholmes <60571002+johnmholmes@users.noreply.github.com>
2025-01-11 03:00:00 +0000

    Update ESP32_13Outputs_NoCan.ino

--- Examples/ESP32/ESP32_13Outputs_NoCan/ESP32_13Outputs_NoCan.ino
+++ Examples/ESP32/ESP32_13Outputs_NoCan/ESP32_13Outputs_NoCan.ino
@@ -4,10 +4,31 @@
    each:
    -- pin number from 21,22,15,2,4,16,17,13,12,14,26,25,33
    -- Actions from: Low, High, Flash, Double Strobe, Random, Fire
-      FIre uses two channels.
-   -- two parameters: durn and rate
-      -- each in steps of 100ms
-      == for fire, these determing the randomness of the two channels. 
+
+  EFFECTS:
+  None
+  Low: pulls the pin low (0V), the parameters make no change
+  High: pushes the pin high (3.3V), the parameters make no change
+  Flash: alternates between low and high
+    Parameter 1: sets how long the pin is high (100-25.5 sec)
+    Parameter 2: sets ho long the pin is low (100-25.5 sec)
+  Double strobe: puts the pin high, then low, then high, and then low.
+    Parameter 1: how long the first low is held (100-25.5 sec)
+    Parameter 2: how long the second low is held (100-25.5 sec)
+  Random: alternates the pin between high and low
+    Parameter 1: a random period the pin in held high, between 20ms and 20min
+    Parameter 2: a random period the pin in held low, between 20ms and 20min
+    Note the random period is between the half and the whole of the chosen period.
+    Eg: if the chosen period is 30 sec, then the random periods will be 
+        between 25 sec and 30 sec.  
+    This should work well for house lights for example.  
+  Fire: this uses two consecutive pins. 
+    It uses analogWtite to set the brightness of two LEDs attached to the pins.
+    You may have to choose higher values to see an effect, as values below a 
+    threshold may not light the LED.  
+    Parameter 1: sets the randomness of the first pin of the pair 
+    Parameter 2: sets the randomness of the second pin of the pair 
+
 */
 
 //   David Harris 2019, adapted from
@@ -114,8 +135,14 @@ extern "C" {                      // the following are defined as external
                   <relation><property>6</property><value>Fire (uses two pins)</value></relation>
                 </map>
             </int>
-            <int size="1"><name>Duration (100 ms)</name></int>
-            <int size="1"><name>Rate (100 ms)</name></int>
+            <int size="1">
+              <name>Parameter 1</name>
+              <hints><slider tickSpacing='50' immediate='yes' showValue='yes'> </slider></hints>
+            </int>
+            <int size="1">
+              <name>Parameter 2</name>
+              <hints><slider tickSpacing='50' immediate='yes' showValue='yes'> </slider></hints>
+            </int>
           </group>          
        )" CDIfooter;
        // ^^^^^ Enter User definitions above CDIfooter line ^^^^^
@@ -382,11 +409,17 @@ void doActions() {
       }
       break;
     case aFIRE:
+      if(state[i] != 0) return; 
+      if(durn==0) {
+        analogWrite(pin[pi], 0);
+        analogWrite(pin[pi+1], 0);
+        state[i] = 1;
+      }
       if( timer[i] && (now-timer[i])>0 ) {
-        dP("\naFIRE");
-        if(pi+1>NUM_CHANNEL) return;
-        analogWrite(pin[pi], random(0,durn));
-        analogWrite(pin[pi+1], random(0,rate));
+        dP("\naFIRE"); dP(pin[pi]);
+        if(pi+1>NUM_CHANNEL) break;
+        analogWrite(pin[pi], random(durn));
+        analogWrite(pin[pi+1], random(rate));
         timer[i] = now+15;
       }
       break;
